a4629b68997423d6ec1b4d46b9e66e3d0410f47c
Daniel Kandalaft <kandddany1985@gmail.com>
2023-07-02 20:59:01 +0300

    Assignment 1 - Writing first React Code

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // ReactDOM.render(<p>Hello World from React</p>, document.getElementById("root"));
+
+/*
+Display the following text
+
+Add a div and inside there add h1 and p tag
+h1 -> React Course
+p -> We will learn react in this course.
+*/
+
 ReactDOM.render(
   <div>
     <h1>React Course</h1>
