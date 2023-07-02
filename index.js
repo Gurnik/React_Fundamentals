eef9f4e6d24b8a03944197ae7294f19d2c18bf71
Daniel Kandalaft <kandddany1985@gmail.com>
2023-07-02 21:12:17 +0300

    Fixed error by updating <script src="index.js" type="text/javascript"></script> to <script src="index.js" type="text/babel"></script>

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <div>
+//     <h1>React Course</h1>
+//     <p>We will learn react in this course.</p>
+//   </div>
+// );
